6ae27d17687282765c468b99f7864fbfe708b90d
micmic17 <79235904+micmic17@users.noreply.github.com>
2021-02-18 18:11:26 +0800

    Revert "Add Role-User Seeder"

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,17 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $roles = ['admin', 'users'];
-
-        factory(App\Role::class, 2)->create()->each(function($role, $index) use($roles) {
-            $role->name = $roles[$index];
-            $role->save();
-        });
-
-        factory(App\User::class, 1)->create([
-            'name' => 'Admin User',
-            'email' => 'admin@admin.com',
-            'role_id' => 1
-        ]);
+        // $this->call(UserSeeder::class);
     }
 }
