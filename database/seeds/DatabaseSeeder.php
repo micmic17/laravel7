aab0db033a496e824d814e0eb6d2e14eb9f8220d
Micmic <mickale_saturre@ligph.com>
2021-02-18 17:57:57 +0800

    Add Role-User Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $roles = ['admin', 'users'];
+
+        factory(App\Role::class, 2)->create()->each(function($role, $index) use($roles) {
+            $role->name = $roles[$index];
+            $role->save();
+        });
+
+        factory(App\User::class, 1)->create([
+            'name' => 'Admin User',
+            'email' => 'admin@admin.com',
+            'role_id' => 1
+        ]);
     }
 }
