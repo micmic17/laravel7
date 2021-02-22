81757798924163fd080bd84212d6c0b2d8ebc800
Micmic <mickale_saturre@ligph.com>
2021-02-22 10:35:46 +0800

    Added Edit Profile

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Input;
 
 class HomeController extends Controller
 {
@@ -24,5 +26,35 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    public function profile()
+    {
+        $user = Auth::user();
+        
+        return view('user.profile', compact('user'));
+    }
+
+    public function update(Request $request)
+    {
+        $storeImage = $this->storeImage($request);
+
+        $user = Auth::user();
+        $user->update($storeImage);
+
+        return redirect('/profile');
+    }
+
+    protected function storeImage($request)
+    {
+        $file = $request->file('image');
+
+        if ($file) {
+            $path = $file->store('public');
+
+            $request->request->add(['profile_image' => str_replace('public/', '', $path)]);
+        }
+
+        return $request->except(['image']);
     }
 }
--- public/js/app.js
+++ public/js/app.js
@@ -37393,15 +37393,27 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /***/ }),
 
+/***/ "./resources/sass/user/profile.scss":
+/*!******************************************!*\
+  !*** ./resources/sass/user/profile.scss ***!
+  \******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
 /***/ 0:
-/*!*************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
-  \*************************************************************/
+/*!************************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/user/profile.scss ***!
+  \************************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! /Users/mickalesaturre/Desktop/Work/Projects/laravel7/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/mickalesaturre/Desktop/Work/Projects/laravel7/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/mickalesaturre/Desktop/Work/Projects/laravel7/resources/sass/app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! /Users/mickalesaturre/Desktop/Work/Projects/laravel7/resources/sass/user/profile.scss */"./resources/sass/user/profile.scss");
 
 
 /***/ })
